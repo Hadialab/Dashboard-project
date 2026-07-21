7622ea262d24731a1c00369af335d4037e4ec6c8
Hadi Al Abbassi <hadialabbasi01@gmail.com>
2026-07-21 11:00:32 +0300

    Integrate JSON Server, cookie authentication, and reports API

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3001",
-    headers:{
-        "Content-Type": "application/json",
-    },
+  baseURL: "https://dashboard-project-ryn2.onrender.com",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default api;
